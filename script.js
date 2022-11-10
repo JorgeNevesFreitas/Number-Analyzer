6c8f2b2d40ffa72f8dfdd8c246b304d5a40a7747
JorgeNevesFreitas <87647137+JorgeNevesFreitas@users.noreply.github.com>
2022-11-10 11:02:24 +0000

    main commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let number = document.querySelector("input#txtnum");
+let sel = document.querySelector("select#sel");
+let res = document.querySelector("p#res");
+let list = [];
+
+
+
+
+function add() {
+    let n = number.value;
+    
+
+    if (n < 1 || n > 100 || list.includes(n)) {
+        alert("Invalid Value or already added to the list");
+    } else {
+        list.push(n);
+        list.sort();
+        let item = document.createElement("option");
+        item.text = `Number ${n} added`; 
+        sel.appendChild(item);
+        res.innerHTML = ""
+               
+    }
+    number.value = "";
+    number.focus();
+  }
+
+
+
+function analyze() {
+    
+    let total = list.length;
+    let max = list[list.length - 1];
+    let min = list[0];
+    let sum = 0;
+    for (let i = 0; i < list.length; i++) {
+        sum += list[i];
+    }
+    let avg = sum / list.legth;
+
+    res.innerHTML = "";
+    res.innerHTML += `In total, we have ${total} numbers added <br><br>`;
+    res.innerHTML += `The highest number is ${min} <br><br>`;
+    res.innerHTML += `The lowest number is ${max} <br><br>`;
+    res.innerHTML += `The sum of all values ${sum} <br><br>`;
+    res.innerHTML += `The average is ${avg} <br><br>`;
+    
+      }
+    
+    
+
+     console.log(list);
+
+
+  
+
